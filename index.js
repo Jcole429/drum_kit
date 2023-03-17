1f408342e7ae5d14586987d95cc8560868d86bab
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-17 14:32:01 -0400

    Refactor on click event listener

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
 var buttons = document.querySelectorAll(".drum");
 var audioFileSource = "sounds/";
 
+// Detecting button click
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
-        switch (this.innerHTML) {
-            case "w":
-                var audio = new Audio(audioFileSource + "tom-1.mp3");
-                audio.play();
-                break;
-            case "a":
-                var audio = new Audio(audioFileSource + "tom-2.mp3");
-                audio.play();
-                break;
-            case "s":
-                var audio = new Audio(audioFileSource + "tom-3.mp3");
-                audio.play();
-                break;
-            case "d":
-                var audio = new Audio(audioFileSource + "tom-4.mp3");
-                audio.play();
-                break;
-            case "j":
-                var audio = new Audio(audioFileSource + "snare.mp3");
-                audio.play();
-                break;
-            case "k":
-                var audio = new Audio(audioFileSource + "crash.mp3");
-                audio.play();
-                break;
-            case "l":
-                var audio = new Audio(audioFileSource + "kick-bass.mp3");
-                audio.play();
-                break;
-            default:
-                console.log("Unknown button: " + this.innerHTML);
-        }
+        makeSound(this.innerHTML);
     });
+}
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio(audioFileSource + "tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio(audioFileSource + "tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio(audioFileSource + "tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio(audioFileSource + "tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio(audioFileSource + "snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio(audioFileSource + "crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio(audioFileSource + "kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            console.log("Unknown button: " + this.innerHTML);
+    }
 }
